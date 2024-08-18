e75f630279ec6aa8325a991f030a7b63cae7aab0
nagi49000 <nagi49000>
2024-08-18 20:24:35 +0100

    got fastapi app running and plumbed in all I can see for opentel docs - nothing in elastic

--- fastapi-app/api/api/api_app.py
+++ fastapi-app/api/api/api_app.py
@@ -1,2 +1,20 @@
 from .fastapi_app import create_app
+# see https://tillepille.io/posts/otel-fastapi/ since the FastAPIInstrumentor docs are not up to standard
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace import TracerProvider
+
 app = create_app()
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter()
+# we don't want to export every single trace by itself but rather batch them
+span_processor = BatchSpanProcessor(otlp_exporter)
+otlp_tracer = trace.get_tracer_provider().add_span_processor(span_processor)
+
+FastAPIInstrumentor(app, tracer_provider=otlp_tracer)
